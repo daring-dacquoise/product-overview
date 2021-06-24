e08aae3ba4e6b8a0127bbdbf19c20737bd8a425b
Iris <irisxjiang@gmail.com>
2021-06-24 12:38:11 -0700

    update and confirm mySQL ETL commands

---
+++ mySQL_ETL_Commands.sql
@@ -0,0 +1,77 @@
+-- ---
+-- Table 'related_products'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/related.csv'
+    --  -> INTO TABLE related_products
+    --  -> FIELDS TERMINATED BY ','
+    --  -> ENCLOSED BY '"'
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
+
+
+-- ---
+-- Table 'products'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/products.csv'
+    --  -> INTO TABLE products
+    --  -> FIELDS TERMINATED BY ','
+    --  -> ENCLOSED BY '"'
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
+
+-- ---
+-- Table 'styles'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/styles.csv'
+    --  -> INTO TABLE styles
+    --  -> FIELDS TERMINATED BY ','
+    --  -> ENCLOSED BY '"'
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
+
+-- ---
+-- Table 'skus'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/skus.csv'
+    --  -> INTO TABLE skus
+    --  -> FIELDS TERMINATED BY ','
+    --  -> ENCLOSED BY '"'
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
+
+-- ---
+-- Table 'features'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/features.csv'
+    --  -> INTO TABLE features
+    --  -> FIELDS TERMINATED BY ','
+    --  -> ENCLOSED BY '"'
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
+
+-- ---
+-- Table 'related_products'
+-- ---
+
+-- mysql>
+  -- LOAD DATA INFILE '/Users/devbook/HackReactor/SDC/product-overview/photos.csv'
+    --  -> INTO TABLE photos
+    --  -> FIELDS TERMINATED BY ','
+    --  -> LINES TERMINATED BY '\n'
+    --  -> IGNORE 1 ROWS
+    --  -> ;
