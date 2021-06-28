5ad94b7e3ea86b14cec337b0c9895bf7f5c12ee4
Iris <irisxjiang@gmail.com>
2021-06-28 10:42:00 -0700

    frisby tests for all requests

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -1,6 +1,25 @@
 const frisby = require('frisby');
 
-it('should be a teapot', function() {
-  return frisby.get('http://httpbin.org/status/418')
-    .expect('status', 418);
+it('GET all products should return status of 200 OK', function() {
+  return frisby
+    .get('http://localhost:3000/products')
+    .expect('status', 200);
+});
+
+it('GET one product detail should return status of 200 OK', function() {
+  return frisby
+    .get('http://localhost:3000/products/123')
+    .expect('status', 200);
+});
+
+it('GET styles for one product should return status of 200 OK', function() {
+  return frisby
+    .get('http://localhost:3000/products/1234/styles')
+    .expect('status', 200);
+});
+
+it('GET related items for one product should return status of 200 OK', function() {
+  return frisby
+    .get('http://localhost:3000/products/88/related')
+    .expect('status', 200);
 });
