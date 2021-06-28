dc8ab170efe5442e86e8971735b5333654763c02
Iris <irisxjiang@gmail.com>
2021-06-28 16:34:42 -0700

    k6 tests for all endpoints, add test start scripts

---
+++ k6_tests/getAllProducts.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 30 },
+    { duration: '1ms', target: 1000 },
+    { duration: '20s', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.1'],
+    http_req_duration: ['p(95)<250'],
+  }
+};
+
+//init code
+
+export default function () {
+  //vu code
+  const res =  http.get(`http://localhost:3000/products/`);
+
+  check(res, {
+    'response status is 200': (res) => res.status === 200,
+    'response duration is < 250ms': (res) => res.timings.duration < 250,
+  });
+
+  sleep(1);
+
+}
