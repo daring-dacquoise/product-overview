d38220f012a41dad23589612befb4868c8020b68
Iris <irisxjiang@gmail.com>
2021-06-28 13:02:28 -0700

    k6 for get all products

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+import { Rate } from 'k6/metrics';
+
+
+
 export let options = {
-  vus: 10,
+  vus: 20,
   duration: '30s',
 };
+
+export let errorRate = new Rate('errors');
 
 //init code
 
 export default function () {
   //vu code
 
-  http.get('https://test.k6.io');
+  let url = 'http://localhost:3000/products/';
+
+  check(http.get(url), {
+    'status is 200': (r) => r.status === 200,
+  }) || errorRate.add(1);
   sleep(1);
+
 }
