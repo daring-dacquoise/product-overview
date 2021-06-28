aac1cc34b41e0e3313fb04b2204ce15f1a29a9de
Iris <irisxjiang@gmail.com>
2021-06-28 12:02:19 -0700

    update frisby test with Joi to expect JSON types

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -1,25 +1,65 @@
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
-it('GET all products should return status of 200 OK', function() {
-  return frisby
-    .get('http://localhost:3000/products')
-    .expect('status', 200);
-});
-
-it('GET one product detail should return status of 200 OK', function() {
+it('GET one product should return product details including features for one product with status of 200', function() {
   return frisby
     .get('http://localhost:3000/products/123')
-    .expect('status', 200);
+    .expect('status', 200)
+    .expect('jsonTypes', {
+      id: Joi.number(),
+      name: Joi.string(),
+      slogan: Joi.string(),
+      description: Joi.string(),
+      category: Joi.string(),
+      default_price: Joi.number(),
+      features: Joi.array().items(
+        Joi.object({
+          feature: Joi.string(),
+          value: Joi.string()
+        })
+      )
+    })
 });
 
-it('GET styles for one product should return status of 200 OK', function() {
+it('GET styles should return styles including photos and skus for one product with status of 200', function() {
   return frisby
     .get('http://localhost:3000/products/1234/styles')
-    .expect('status', 200);
+    .expect('status', 200)
+    .expect('jsonTypes', {
+      product_id: Joi.number(),
+      results: Joi.array().items(
+        Joi.object({
+          style_id: Joi.string(),
+          name: Joi.string(),
+          original_price: Joi.number(),
+          sale_price: Joi.string(),
+          photos: Joi.array().items(
+            Joi.object({
+              thumbnail: Joi.string(),
+              url: Joi.string(),
+            })
+          )
+        })
+      )
+    });
 });
 
 it('GET related items for one product should return status of 200 OK', function() {
   return frisby
     .get('http://localhost:3000/products/88/related')
-    .expect('status', 200);
+    .expect('status', 200)
+});
+
+it('GET all products should return a list of products with status 200', function() {
+  return frisby
+    .get('http://localhost:3000/products')
+    .expect('status', 200)
+    .expect('jsonTypes', '*', {
+      id: Joi.number(),
+      name: Joi.string(),
+      slogan: Joi.string(),
+      description: Joi.string(),
+      category: Joi.string(),
+      default_price: Joi.number(),
+    })
 });
