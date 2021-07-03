336a05c30355124ee4f4d2e6d69dfcad7c6c33da
Iris <irisxjiang@gmail.com>
2021-07-03 15:35:41 -0700

    set up loader.io testing

--- server/index.js
+++ server/index.js
@@ -4,22 +4,26 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
+const path = require('path');
 const router = require('./routes.js');
 const db = require('../database');
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'testing', 'loader_test')));
+
 app.use(router);
 
 //only bring server up if connected to db
 
-db.connectDb((err) => {
-  if (!err) {
+// db.connectDb((err) => {
+  // if (!err) {
     app.listen(PORT, () => {
       console.log(`hi server is listening on port: ${PORT}`);
     });
-  }
-});
+  // }
+// });
 
 // app.listen(PORT, () => {
 //   console.log(`server is listening on port: ${PORT}`);
